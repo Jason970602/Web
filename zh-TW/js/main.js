875f22e7fa1da99fcc90859166062772196f8fbf
michael6856 <64688903+michael6856@users.noreply.github.com>
2023-08-02 16:27:58 +0800

    7636page

---
+++ zh-TW/js/main.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+let slides = document.querySelectorAll('.slideshow img');
+let slideInterval = setInterval(nextSlide, 4000);
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+new fullpage('#fullpage', {
+    controlArrows: false,
+    navigation: true,
+    slidesNavigation: true
+})
